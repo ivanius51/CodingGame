a010f6735ccfe0a033f84dc53b41302f2b54690b
ivanius51 <ivanius51@gmail.com>
2018-07-01 18:48:50 +0300

    no message

--- Rugby score.cpp
+++ Rugby score.cpp
@@ -2,8 +2,37 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
+
+void penaltyDrops(int N, string sofar)
+{
+  if (N % 3 != 0)
+  {
+    return;
+  }
+  cout << sofar << N / 3 << endl;
+}
+void transformations(int N, int sofar)
+{
+  int tries = min(sofar, N / 2);
+  for (int i = 0; i <= tries; i++)
+  {
+    char buff[255];
+    snprintf(buff, sizeof(buff), "%d %d ", sofar, i);
+    std::string sofarstr = buff;
+    penaltyDrops(N - i * 2, sofarstr);
+  }
+}
+void tries(int N)
+{
+  int tries = N / 5;
+  for (int i = 0; i <= tries; i++)
+  {
+    transformations(N - i * 5, i);
+  }
+}    
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -11,12 +40,13 @@ using namespace std;
  **/
 int main()
 {
-    int N;
-    cin >> N; cin.ignore();
+  int N;
+  cin >> N; cin.ignore();
 
-    cerr << N << endl;
-    // Write an action using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
+  cerr << N << endl;
+  // Write an action using cout. DON'T FORGET THE "<< endl"
+  // To debug: cerr << "Debug messages..." << endl;
 
-    cout << "tries transformations penalties" << endl;
+  tries(N);
 }
+
