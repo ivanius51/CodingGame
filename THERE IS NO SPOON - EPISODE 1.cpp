71f22d71569f9e77be6bcee964e37a2fb424cfa6
ivanius51 <ivanius51@gmail.com>
2018-07-07 16:16:04 +0300

    THERE IS NO SPOON - EPISODE 1 - 100%

--- THERE IS NO SPOON - EPISODE 1.cpp
+++ THERE IS NO SPOON - EPISODE 1.cpp
@@ -5,36 +5,92 @@
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+int width; // the number of cells on the X axis
+int height; // the number of cells on the Y axis
+std::vector<std::string> lines;
+
+void printRight(int px, int py)
+{
+  for (int x = px + 1; x < width; ++x)
+  {
+    if (lines[py][x] == '0')
+    {
+      cout << x << " " << py;
+      return;
+    }
+  }
+  cout << "-1 -1";
+  /*
+  for (int x = px - 1; x > 0; --x)
+  {
+    if (lines[py][x] == '0')
+    {
+      cout << x - px << " " << py;
+      break;
+    }
+  }
+  */
+}
+void printBottom(int px, int py)
+{
+  for (int y = py + 1; y < height; ++y)
+  {
+    if (lines[y][px] == '0')
+    {
+      cout << px << " " << y;
+      return;
+    }
+  }
+  cout << "-1 -1";
+  /*
+  for (int y = py - 1; y > 0; --y)
+  {
+    if (lines[y][px] == '0')
+    {
+      cout << px << " " << y - py;
+      break;
+    }
+  }
+  */
+}
+
 int main()
 {
-    int surfaceN; // the number of points used to draw the surface of Mars.
-    cin >> surfaceN; cin.ignore();
-    for (int i = 0; i < surfaceN; i++) {
-        int landX; // X coordinate of a surface point. (0 to 6999)
-        int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-        cin >> landX >> landY; cin.ignore();
+  cin >> width; cin.ignore();
+  cin >> height; cin.ignore();
+  lines.resize(height);
+  for (int y = 0; y < height; y++)
+  {
+    //string line; // width characters, each either 0 or .
+    getline(cin, lines[y]);
+    cerr << lines[y] << endl;
+    /*
+    for (int x = 0; x < width; ++x)
+    {
+      if (lines[y][x] == '0')
+      {
+        //cerr << "x=" << x << " y=" << y << endl;
+      }
     }
-
-    // game loop
-    while (1) {
-        int X;
-        int Y;
-        int hSpeed; // the horizontal speed (in m/s), can be negative.
-        int vSpeed; // the vertical speed (in m/s), can be negative.
-        int fuel; // the quantity of remaining fuel in liters.
-        int rotate; // the rotation angle in degrees (-90 to 90).
-        int power; // the thrust power (0 to 4).
-        cin >> X >> Y >> hSpeed >> vSpeed >> fuel >> rotate >> power; cin.ignore();
-
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-
-        // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
-        cout << "0 3" << endl;
+    */
+  }
+  for (int y = 0; y < height; ++y)
+  {
+    for (int x = 0; x < width; ++x)
+    {
+      if (lines[y][x] == '0')
+      {
+        cerr << "x=" << x << " y=" << y << endl;
+        cout << x << " " << y;
+        cout << " ";
+        printRight(x, y);
+        cout << " ";
+        printBottom(x, y);
+        cout << endl;
+      }
     }
+  }
+
+  //cout << "0 0 1 0 0 1" << endl;
+  //cout << "1 0 -1 -1 -1 -1" << endl;
 }
